775a156327b74b1ecfe07029b299ce0dd95b3fd7
Ксения Тихомирова <tuxuu.mup@gmail.com>
2025-12-17 23:26:43 +0500

    Add project files.

---
+++ Tyuiu.TikhomirovaKA.Sprint7.Project.V12.Lib/DataService.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.TikhomirovaKA.Sprint7.Project.V12.Lib
+{
+    public class DataService
+    {
+
+    }
+}
---
+++ Tyuiu.TikhomirovaKA.Sprint7.Project.V12.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.TikhomirovaKA.Sprint7.Project.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
