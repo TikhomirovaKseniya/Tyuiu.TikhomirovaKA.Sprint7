6affa145d81fa967ef348eaf7caceb29629ff4b9
Ксения Тихомирова <tuxuu.mup@gmail.com>
2025-12-17 23:29:12 +0500

    Commit1

--- Tyuiu.TikhomirovaKA.Sprint7.Project.V12/FormMain.cs
+++ Tyuiu.TikhomirovaKA.Sprint7.Project.V12/FormMain.cs
@@ -1,7 +1,10 @@
+using Tyuiu.TikhomirovaKA.Sprint7.Project.V12.Lib;
+
 namespace Tyuiu.TikhomirovaKA.Sprint7.Project.V12
 {
     public partial class Form1 : Form
     {
+        DataService ds = new DataService();
         public Form1()
         {
             InitializeComponent();
