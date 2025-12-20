cae7e388b0207f461c583c8bf0ee7d208aa17022
Ксения Тихомирова <tuxuu.mup@gmail.com>
2025-12-20 15:37:29 +0500

    Added Library

--- Tyuiu.TikhomirovaKA.Sprint7.Project.V12.Lib/DataService.cs
+++ Tyuiu.TikhomirovaKA.Sprint7.Project.V12.Lib/DataService.cs
@@ -1,7 +1,27 @@
-﻿namespace Tyuiu.TikhomirovaKA.Sprint7.Project.V12.Lib
+﻿using System.Data;
+
+namespace Tyuiu.TikhomirovaKA.Sprint7.Project.V12.Lib
 {
     public class DataService
     {
-
+        public DataTable ReadCsvToDataTable(string filePath)
+        {
+            DataTable dataTable = new DataTable();
+            string[] lines = File.ReadAllLines(filePath);
+            if (lines.Length > 0)
+            {
+                string[] header = lines[0].Split(';');
+                foreach (var column in header)
+                {
+                    dataTable.Columns.Add(column);
+                }
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string[] row = lines[i].Split(';');
+                    dataTable.Rows.Add(row);
+                }
+            }
+            return dataTable;
+        }
     }
 }
